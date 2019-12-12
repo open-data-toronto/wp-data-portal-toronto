ee6b945331120dfdaea6c8efcd80181acb6e16a6
dottyz <yizhao.tan@gmail.com>
2019-12-12 15:15:58 -0500

    Update for internal styling

--- wp-open-data-toronto/header.php
+++ wp-open-data-toronto/header.php
@@ -68,6 +68,7 @@
                 <a class="navbar-brand" href="<?php echo home_url(); ?>">
                   Open Data
                 </a>
+                <div class="internal-badge"><i class="fa fa-lock"></i> Internal</div>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
